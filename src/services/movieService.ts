0cda93cb8d7adc6455590f68f22dfca7216e63b6
Dobryak96 <gorokhov_olexandr@ukr.net>
2026-08-13 22:09:03 +0100

    back

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,32 +1,28 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-export interface TMDBResponse {
+  export const API = axios.create({
+  baseURL: `${import.meta.env.VITE_TMDB_URL}`,
+  headers: { Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` },
+});
+
+interface FetchMoviesResponse {
   page: number;
   results: Movie[];
   total_pages: number;
-  total_results: number;
 }
-
-const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-
-export const fetchMovies = async (
+const fetchMovies = async (
   query: string,
-  page: number = 1,
-): Promise<TMDBResponse> => {
-  const token = import.meta.env.VITE_TMDB_TOKEN;
-
-  const response = await axios.get<TMDBResponse>(BASE_URL, {
+  page: number,
+): Promise<FetchMoviesResponse> => {
+  const response = await API.get<FetchMoviesResponse>("/search/movie", {
     params: {
       query,
-      include_adult: false,
-      language: "en-US",
       page,
     },
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
   });
-
   return response.data;
 };
+
+export default fetchMovies;
+
